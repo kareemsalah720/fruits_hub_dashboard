bc6a27c448ee1c86b0ba0243688fdd1a3c9016ba
Kareem Salah <kareemsalah720@gmail.com>
2025-05-13 06:10:11 +0300

    Configure supabase

--- lib/core/utils/backend_endpoint.dart
+++ lib/core/utils/backend_endpoint.dart
@@ -1,7 +1,7 @@
 class BackendEndpoint {
   static const images = 'images';
   static const productsCollection = 'add_products';
-  static const addUserData = 'users';
-  static const getUsersData = 'users';
-  static const isUserExists = 'users';
+  static const subabaseUrl = 'https://kvimjoliawtxykrfimey.supabase.co';
+  static const supabaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2aW1qb2xpYXd0eHlrcmZpbWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMDQzMzAsImV4cCI6MjA2MjY4MDMzMH0.UaL_qkC5gyXmnlGE7fTdtQac0mTyUQUt9gSHcBopOKw';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_dashboard/core/routes/on_generate_routes.dart';
 import 'package:fruits_hub_dashboard/core/services/custom_bloc_observer.dart';
 import 'package:fruits_hub_dashboard/core/services/get_it_service.dart';
+import 'package:fruits_hub_dashboard/core/utils/backend_endpoint.dart';
 import 'package:fruits_hub_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
 import 'package:fruits_hub_dashboard/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+   await Supabase.initialize(
+    url: BackendEndpoint.subabaseUrl,
+    anonKey: BackendEndpoint.supabaseKey,
+  );
   Bloc.observer = CustomBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
